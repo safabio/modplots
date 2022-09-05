1a488e01acc23e29d17ec76d9d058469593485f8
Fabio Sacher <sacfab01@service06.cluster.bc2.ch>
2022-09-05 14:53:39 +0200

    add gnames data set and documentation

---
+++ R/gnames.R
@@ -0,0 +1,21 @@
+#' Gallus gallus gene ID and names table (gg6)
+#'
+#' A data frame of chicken genes and IDs to annotate plots. Created as following:
+#'
+#' gnames <- read.delim("~/Neuraltube/gg6_genes.txt")
+#'    i <- sapply(gnames, is.factor)
+#'    gnames[i] <- lapply(gnames[i], as.character)
+#'    for (i in 1:dim(gnames)[[1]]) {
+#'      ifelse(gnames[i,"Gene.name"] == "",
+#'             gnames[i,"Gene.name"] <- gnames[i,"Gene.stable.ID"],
+#'             gnames[i,"Gene.name"] <- gnames[i,"Gene.name"])
+#'    }
+#'
+#' @format A data frame with 24356 rows and 2 variables:
+#' \describe{
+#'   \item{Gene.stable.ID}{Ensembl ID for chicken genes}
+#'   \item{Gene.name}{corresponding Names, ID if no given name}
+#'   ...
+#' }
+#' @source \url{http://www.https://www.ensembl.org/index.html}
+"gnames"
